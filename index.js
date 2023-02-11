e1c5bf93ba5deb5d9f665b60a57b36d196923f62
Juan Pablo Marin Perea <1005744966@u.icesi.edu.co>
2023-02-10 19:04:38 -0500

    feat: lab1

---
+++ index.js
@@ -0,0 +1,11 @@
+const reverseNumber = require("reverse_array_number");
+const number = reverseNumber([9, 2, 3, 4, 5]);
+
+console.log (number);
+
+///Geocode
+
+const reverse = require('reverse-geocode')
+console.log(reverse.lookup(37.8072792, -122.4780652, 'us'))
+
+console.log (reverse)
